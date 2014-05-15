5b841c2ee3c781b34cb775a4851841470c1fc005
Wouter de Vos <wouter@springest.com>
2014-05-15 10:11:22 +0200

    Basic routes and empty namespaced controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  # Frontend routes
   resources :surveys
   resources :attempts, :only => [:new, :create]
 
+  # Admin routes
+  namespace(:admin) do
+    resources :surveys
+  end
+
+  # Admin Users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  # Frontend Users
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   get '/users/:id/add_email' => 'users#add_email', via: [:get, :patch, :post],
     as: :add_user_email
+
+  # Homepage
   root to: 'contests#new'
 end
