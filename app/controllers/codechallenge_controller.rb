9a23ced8cddb208824f1b47abe614e70cddbfc44
Tom Hoenderdos <info@tompc.nl>
2015-09-10 16:28:12 +0200

    A first version for syntax checking the given input. Implement first crude function to get the next codechallenge

--- app/controllers/codechallenge_controller.rb
+++ app/controllers/codechallenge_controller.rb
@@ -1,10 +1,30 @@
 class CodechallengeController < ApplicationController
 
   def index
-
+    @codechallenge = Codechallenge.first
+    @execute_result = ""
   end
 
   def update
-    
+      current_id = params[:id]
+      attempt = params[:codechallenge_attempt]
+      @codechallenge = Codechallenge.find(current_id)
+      @execute_result = ""
+
+      unless Codechallenge.find(current_id).solution == attempt
+        command = "ruby -c << EOF\r\n#{attempt} \r\nEOF"
+        Open3.popen2e(command){|stdin,out,blub|
+          out.each{ |line|
+            @execute_result += line+"<br/>"
+          }
+        }
+        # TODO: regex the /tmp/file.rb:1 part
+
+        @codechallenge.problem = attempt
+      else
+        @execute_result = "Syntax OK"
+        @codechallenge = @codechallenge.nextChallenge
+      end
+    render :index
   end
 end
--- app/models/codechallenge.rb
+++ app/models/codechallenge.rb
@@ -1,3 +1,7 @@
 class Codechallenge < ActiveRecord::Base
 
+  def nextChallenge(current_id)
+    self.class.where('id > ?', current_id).first
+  end
+
 end
