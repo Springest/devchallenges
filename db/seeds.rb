89e231e2c1c970d0952e83d37070f4d10184dd09
Tom Hoenderdos <info@tompc.nl>
2015-09-10 11:14:28 +0200

    Refactor seed for admin user. include the code of admin_users in the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Add admin_users to the rake db:seeds action
+require_relative "seeds/admin_users.rb"
--- db/seeds/admin_users.rb
+++ db/seeds/admin_users.rb
@@ -1,1 +1,10 @@
-AdminUser.create(:id => 1, :email => "admin@example.com", :encrypted_password => "$2a$10$mw6IeRUR/INTq00.qTsgjuYusz2GYv49mjUN6suFWSrqbPj6BDZNu", :sign_in_count => 1, :current_sign_in_at => Fri, 09 May 2014 13:40:57 UTC +00:00, :last_sign_in_at => Fri, 09 May 2014 13:40:57 UTC +00:00, :current_sign_in_ip => "127.0.0.1", :last_sign_in_ip => "127.0.0.1")
+AdminUser.create(
+      :id => 1,
+      :email => "admin@example.com",
+      :password => "password",
+      :sign_in_count => 1,
+      :current_sign_in_at => Time.now.yesterday,
+      :last_sign_in_at => Time.now,
+      :current_sign_in_ip => "127.0.0.1",
+      :last_sign_in_ip => "127.0.0.1"
+)
