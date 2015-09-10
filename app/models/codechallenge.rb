c5b0ab87ab21445da0b627956248266bf288bb93
Tom Hoenderdos <info@tompc.nl>
2015-09-10 17:47:34 +0200

    Refactor nextChallenge function

--- app/models/codechallenge.rb
+++ app/models/codechallenge.rb
@@ -1,7 +1,9 @@
 class Codechallenge < ActiveRecord::Base
 
-  def nextChallenge(current_id)
-    self.class.where('id > ?', current_id).first
+  def nextChallenge
+    self.class.where('id > ?', id).first
+  end
+
   def compare_problem_to(attempt)
     unless self.class.where(id: id).take.solution == attempt
       command = "ruby -c << EOF\r\n#{attempt} \r\nEOF"
