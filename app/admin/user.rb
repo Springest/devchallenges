a0d8a9c73563b2e11e4a0076e82b8f0039d7d492
Tom Hoenderdos <info@tompc.nl>
2015-09-11 17:55:09 +0200

    Add codechallenge_id to the edit view in admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,12 +1,22 @@
 ActiveAdmin.register User do
 
   index do
-      column :email
-      column :password
-      column :name
-      column :username
-      column :codechallenge_id
-      # default_actions
+    column :email
+    column :encrypted_password
+    column :name
+    column :username
+    column :codechallenge_id
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "" do
+      f.input :email
+      f.input :name
+      f.input :username
+      f.input :codechallenge_id
+    end
+    f.actions
   end
 
   controller do
